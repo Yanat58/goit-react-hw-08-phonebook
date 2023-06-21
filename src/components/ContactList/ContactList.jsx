5c46bb61833a749f6bf16c646a8894a26d76309d
Yanat58 <tanyalyt80@gmail.com>
2023-06-21 15:59:44 +0300

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import css from './ContactList.module.css';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import {
   selectContactValue,
   selectFilterValue,
 } from 'redux/contacts/selectors';
 import { ContactItem } from 'components/ContactItem/ContactItem';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
-import { fetchContacts } from 'redux/contacts/operations';
 
 export const ContactList = () => {
   const contacts = useSelector(selectContactValue);
   const filter = useSelector(selectFilterValue);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      dispatch(fetchContacts());
-    }
-  }, [dispatch, isLoggedIn]);
+  
 
   const filterContactHandler = () => {
     const normalizedFilter = filter.toLocaleLowerCase();
